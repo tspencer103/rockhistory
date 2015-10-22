617998405a8f05b3f1d6493a018a3bf69c1cb284
Tim Spencer <tspencer103@gmail.com>
2015-10-22 10:28:48 -0500

    budweiser.com/rockhistory

---
+++ index.php
@@ -0,0 +1,133 @@
+<html>
+<title>Today in Rock History</title>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+<script>
+$(function() {
+  $.ajax({  
+      type: "POST",  
+      url: "pull_history.php",  
+      data: "" + status,  
+      success: function(data){  
+          document.getElementById("demo").innerHTML = data;
+
+      }  
+  });
+});
+</script>
+
+<script>
+$(document).ready(function()
+{	
+	$(document).on('submit', '#reg-form', function()
+	{
+		
+		var data = $(this).serialize();
+		
+		$.ajax({
+		
+		type : 'POST',
+		url  : 'pull_history.php',
+		data : data,
+		success :  function(data)
+				{		
+				document.getElementById("demo").innerHTML = data;
+				}
+		});
+		return false;
+	});
+});
+
+</script>
+<style type="text/css">
+    body { background: #333333; }
+    p 
+    {
+    font-family: sans-serif;
+    }
+    .Table
+    {
+        display: Table;
+    }
+    .Title
+    {
+        display: table-caption;
+        text-align: center;
+        font-weight: bold;
+        font-size: larger;
+        background: #c6d4a8;
+    }
+    .Heading
+    {
+        display: table-row;
+        font-weight: bold;
+        text-align: center;
+    }
+    .Row
+    {
+        display: table-row;
+    }
+    .Cell
+    {
+        display: table-cell;
+        //border: solid;
+        //border-width: thin;
+        padding: 15px;
+    }
+    div.row:nth-child(odd)
+    {
+	background: #daedb2;
+    }
+    div.row:nth-child(even)
+    {
+	background: #c6d4a8;
+    }
+    div.Container
+    {
+    margin: auto;
+    width: 60%;
+    background: #5e6d3d;
+    padding: 10px;
+    }
+</style>
+
+
+</head>
+
+<body>
+
+<div class="Container">
+<form id="reg-form" name="reg-form" method="post">
+<select name="month"  id="month" placeholder="Month" size="1">
+<?php 
+$mo = 1; 
+while($mo <= 12) {
+    echo '<option value= "' . $mo . '">' . date("F", mktime(0, 0, 0, $mo+1, 0, 0)) . '</option><br>';
+    echo "\n";
+    $mo++;
+} 
+?>
+</select>
+<select name="day" id="day" placeholder="Day" size="1">
+<?php
+$da = 1; 
+while($da <= 31) {
+    echo '<option value= "' . $da . '">' . date("j", mktime(0, 0, 0, 0, $da, 0)) . '</option><br>';
+    echo "\n";
+    $da++;
+} 
+?>
+
+ </select>
+ <button type="submit" >Go</button>
+
+ </form>
+  
+    <p id="demo"></p>
+
+</div>
+
+</body>
+
+</html>
---
+++ pull_history.php
@@ -0,0 +1,48 @@
+<?php
+
+if($_POST) {
+	$month= $_POST['month'];
+	$day= $_POST['day'];
+} else {
+	$month = date('n');
+	$day = date('j');
+}
+	
+$tdate=date("F j", mktime(0, 0, 0, $month, $day, 0));
+
+?>
+<div class="Table">
+    <div class="Title">
+        <p><? echo "This Day in Rock History for $tdate" ?></p>
+    </div>
+    <div class-"heading"> 
+    </div>
+
+<?php
+	$db = mysql_connect("localhost","babewe5_wlup", "lup234");
+	mysql_select_db("babewe5_wlup",$db);
+	$result = mysql_query("SELECT * FROM RockHistory081512 WHERE month=$month AND day=$day ORDER BY year",$db);
+		if (!$result) { echo("ERROR: " . mysql_error() . "\n$SQL\n"); }
+			while ($row = mysql_fetch_array ($result)) {
+			?>
+
+			<div class="Row">
+        			<div class="Cell">
+            				<p><? echo $row["year"] ?></p>
+        			</div>
+        			<div class="Cell">
+            				<p><? echo $row["history"] ?></p>						
+        			</div>
+			</div>
+			<?
+			}
+			mysql_free_result ($result);
+			?>
+
+<div class="Row">
+     <div class="Cell">
+     </div>
+     <div class="Cell">
+           <p><small>Copyright &copy; <? echo date("Y"); ?> Tim Spencer</p>
+     </div>
+</div>
